5470c40aa4f7acc4e0c0dd15767e6ffeee7ca27d
UIowaEABurns <eric-burns@uiowa.edu>
2015-03-24 21:47:49 -0500

    Some early database work

---
+++ src/data/entities/SettingsProfile.swift
@@ -0,0 +1,75 @@
+//
+//  SettingsProfile.swift
+//  AirHockeyProject
+//
+//  Created by divms on 3/24/15.
+//  Copyright (c) 2015 divms. All rights reserved.
+//
+
+import Foundation
+
+public class SettingsProfile {
+    private var id : Int?
+    private var friction : Double?
+    private var playerOnePaddleRadius : Double?
+    private var playerTwoPaddleRadius : Double?
+    private var puckRadius : Double?
+    private var timeLimit : Int?
+    private var goalLimit : Int? // null if they are infinite
+    private var aiDifficulty : Int?
+    
+    init() {
+        
+    }
+    
+    public func getId() -> Int? {
+        return id;
+    }
+    public func setId(id : Int?) {
+        self.id=id
+    }
+    public func getFriction() -> Double? {
+        return friction
+    }
+    public func setFriction(fric : Double?) {
+        self.friction=fric
+    }
+    public func getPlayerOnePaddleRadius() -> Double? {
+        return playerOnePaddleRadius
+    }
+    public func setPlayerOnePaddleRadius(r: Double?) {
+        playerOnePaddleRadius=r
+    }
+    public func getPlayerTwoPaddleRadius() -> Double? {
+        return playerTwoPaddleRadius
+    }
+    public func setPlayerTwoPaddleRadius(r: Double?) {
+        playerTwoPaddleRadius=r
+    }
+    public func getPuckRadius() -> Double? {
+        return puckRadius
+    }
+    public func setPuckRadius(r: Double?) {
+        puckRadius=r
+    }
+    public func getTimeLimit() -> Int? {
+        return timeLimit
+    }
+    public func setTimeLimit(t : Int?) {
+        timeLimit=t
+    }
+    public func getGoalLimit() -> Int? {
+        return goalLimit
+    }
+    public func setGoalLimit(g: Int?) {
+        goalLimit=g
+    }
+    public func getAIDifficulty() -> Int? {
+        return aiDifficulty
+    }
+    public func setAIDifficulty(d: Int? ){
+        aiDifficulty=d
+    }
+    
+    
+}
---
+++ src/data/entities/Stats.swift
@@ -0,0 +1,86 @@
+//
+//  Stats.swift
+//  AirHockeyProject
+//
+//  Created by divms on 3/24/15.
+//  Copyright (c) 2015 divms. All rights reserved.
+//
+
+import Foundation
+
+public class Stats {
+    private var id : Int?
+    private var gamesComplete: Int?
+    private var gamesExited : Int?
+    private var gamesWon : Int?
+    private var gamesLost : Int?
+    private var gamesTied : Int?
+    private var timePlayed : Int?
+    private var goalsScored : Int?
+    private var goalsAgainst : Int?
+    init() {
+        
+    }
+    
+    public func getId() -> Int? {
+        return id
+    }
+    public func getGamesComplete() -> Int? {
+        return gamesComplete
+    }
+    public func getGamesExited() -> Int? {
+        return gamesExited
+    }
+    public func getGamesWon() -> Int? {
+        return gamesWon
+    }
+    public func getGamesLost() -> Int? {
+        return gamesLost
+    }
+    public func getGamesTied() -> Int? {
+        return gamesTied
+    }
+    public func getTimePlayed() -> Int? {
+        return timePlayed
+    }
+    public func getGoalsScored() -> Int? {
+        return goalsScored
+    }
+    public func getGoalsAgainst() -> Int? {
+        return goalsAgainst
+    }
+    
+    
+    
+    
+    public func setId(i : Int?) {
+        self.id=i
+    }
+    public func setGamesComplete(i : Int?) {
+        self.gamesComplete=i
+    }
+    public func setGamesExited(i : Int?) {
+        self.gamesExited=i
+    }
+    public func setGamesWon(i : Int?) {
+        self.gamesWon=i
+    }
+    public func setGamesLost(i : Int?) {
+        self.gamesLost=i
+    }
+    public func setGamesTied(i : Int?) {
+        self.gamesTied=i
+    }
+    
+    public func setTimePlayed(i : Int?) {
+        self.timePlayed=i
+    }
+    
+    public func setGoalsScored(i : Int?) {
+        self.goalsScored=i
+    }
+    public func setGoalsAgainst(i : Int?) {
+        self.goalsAgainst=i
+    }
+    
+}
